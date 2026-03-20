536c4154621de8f3839cda8c0be37a20c7ff9862
NikitaSemerikov <167699682+NikitaSemerikov@users.noreply.github.com>
2026-03-20 12:33:14 +0300

    Семериков_25-ИСТ-3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,58 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t maxv = num;
+
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      if (num > (UINT64_MAX - 1) / 3) {
+        return UINT64_MAX;
+      }
+      num = 3 * num + 1;
+    }
+
+    if (num > maxv) {
+      maxv = num;
+    }
+  }
+
+  return maxv;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  unsigned int len = 1;
+
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      if (num > (UINT64_MAX - 1) / 3) {
+        return len;
+      }
+      num = 3 * num + 1;
+    }
+    len++;
+  }
+
+  return len;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  unsigned int bestNum = static_cast<unsigned int>(lbound);
+  unsigned int bestLen = 0;
+
+  for (uint64_t n = lbound; n <= rbound; n++) {
+    unsigned int len = collatzLen(n);
+    if (len > bestLen) {
+      bestLen = len;
+      bestNum = static_cast<unsigned int>(n);
+    }
+  }
+
+  *maxlen = bestLen;
+  return bestNum;
 }
